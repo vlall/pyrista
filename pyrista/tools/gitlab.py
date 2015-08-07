c358ace3f6fa5735c459e30d7c00609e645b8f7f
Vishal <vlall@users.noreply.github.com>
2015-08-07 00:11:35 -0400

    Create gitlab.py

---
+++ pyrista/tools/gitlab.py
@@ -0,0 +1,16 @@
+import socket
+import commands
+
+class Version_Ctrl:
+	def __init__(self):
+		#if sc == None:
+			#sc = 'git@gitlab.xdataproxy.com:rjacobs/dsra-networking.git'
+		self.status = commands.getstatusoutput('git status')[1]
+		self.changes = 'Untracked files' in self.status
+		self.commit = commands.getstatusoutput('git add .')[1]
+		# git commit -m 'your message here'
+		# git push 
+if __name__ == '__main__':
+	x = Version_Ctrl()
+	print x.changes
+
